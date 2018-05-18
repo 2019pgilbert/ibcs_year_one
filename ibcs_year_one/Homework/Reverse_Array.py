088e37d76f19a8e6526265eef27cb21e0649b93a
Harry Gilbert <2019pgilbert@dwight.edu>
2018-05-18 10:18:04 -0400

    readme

---
+++ ibcs_year_one/Homework/Reverse_Array.py
@@ -0,0 +1,28 @@
+#my code
+newArray = []
+
+def reverseArray(arr): #todo dequeue then push
+    for outLoop in arr:
+        for inLoop in arr:
+            if arr == arr.empty:
+                print("Array reversed")
+                return newArray
+            elif ( arr != arr.empty ):
+                oldItem = arr[0]
+                minusLast = arr.split[0]
+                newArray.extend(oldItem)
+
+reverseString("5", "8", "4", "2", "9")
+
+#def thing(a)
+#a = [1,2,3,4,5]
+#reverse_a = a[::-1]
+
+
+#Teachers code
+def reverseArray(str):
+    new_str = ""
+    for i in string:
+        new_str = i + new_str
+    print (new_str)
+    return new_str
---
+++ ibcs_year_one/Review_Work/yelp.py
@@ -0,0 +1,23 @@
+#datatypes = price, type of food, weather based likelihood
+# hash price = $$$, type of food = pizza, weather = sunny
+# three arrays, price, food, weather
+
+class Yelp():
+
+    def __init__(self):
+        self.food = []
+        self.price = []
+        self.weather = []
+        self.name = name 
+
+    def add_data(self):
+        what_to_add = input("Do you want to add food, price, or weateher?")
+        if what_to_add == 'food'
+            what_food = input('What food would you like to add?')
+            self.food.apend(what_food)
+            #blt taco, hard taco, soft taco
+
+
+# calling my datatypes
+
+taco_bell = Yelp('taco bell')
